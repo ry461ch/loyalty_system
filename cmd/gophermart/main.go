71332668d6ed84b43787e1bbea0be1d2297b0636
ry461ch <ryabov.kolya20013@gmail.com>
2024-09-22 22:59:02 +0700

    add server

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import (
+	"github.com/ry461ch/loyalty_system/internal/config"
+	"github.com/ry461ch/loyalty_system/internal/app"
+)
+
+func main() {
+	server := server.NewServer(config.New())
+	server.Run()
+}
